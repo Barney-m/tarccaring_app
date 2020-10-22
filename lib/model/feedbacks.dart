cf97ea66c6bc423081472b1eac97f1399ce6e88a
ks89 <52287833+Barney-m@users.noreply.github.com>
2020-10-22 23:31:46 +0800

    22/10/2020

--- lib/model/feedbacks.dart
+++ lib/model/feedbacks.dart
@@ -1,51 +1,11 @@
 class Feedbacks {
-  String feedback_id, creator_id, handler_id, status;
-  int feedbackType_id;
+  String creator_id, handler_id, status, feedback_type, choice, comment, attachment;
+  int feedback_id, feedbackType_id;
   bool anonymous;
-  DateTime created_date, approve_date, dismiss_date, last_modified;
+  DateTime created_at, approved_date, dismiss_date, updated_at;
 
-  Feedbacks(
-      this.feedback_id,
-      this.feedbackType_id,
-      this.creator_id,
-      this.handler_id,
-      this.anonymous,
-      this.created_date,
-      this.approve_date,
-      this.dismiss_date,
-      this.last_modified,
-      this.status);
+  Feedbacks(this.feedback_id,this.feedbackType_id, this.choice, this.comment, this.attachment, this.creator_id,this.handler_id,this.anonymous, this.status, this.created_at, this.updated_at, this.approved_date,this.dismiss_date);
+  Feedbacks.withType(this.feedback_id,this.feedbackType_id, this.choice, this.comment, this.attachment, this.creator_id,this.handler_id,this.anonymous, this.status, this.created_at, this.updated_at, this.approved_date,this.dismiss_date, this.feedback_type);
 
-  Feedbacks.approvedContent(
-      this.feedback_id,
-      this.feedbackType_id,
-      this.creator_id,
-      this.anonymous,
-      this.created_date,
-      this.approve_date,
-      this.status);
-
-  Feedbacks.dismissedContent(
-      this.feedback_id,
-      this.feedbackType_id,
-      this.creator_id,
-      this.anonymous,
-      this.created_date,
-      this.approve_date,
-      this.status);
-
-  Map<String, dynamic> toMap() {
-    return {
-      'feedback_id': feedback_id,
-      'feedbackType_id': feedbackType_id,
-      'creator_id': creator_id,
-      'handler_id': handler_id,
-      'anonymous': anonymous,
-      'created_date': created_date,
-      'approve_date': approve_date,
-      'dismiss_date': dismiss_date,
-      'last_modified': last_modified,
-      'status': status,
-    };
-  }
+  
 }
