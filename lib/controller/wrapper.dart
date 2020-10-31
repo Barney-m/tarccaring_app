8b3b46e74529530208b9d63106c3e3209093d50a
ks89 <52287833+Barney-m@users.noreply.github.com>
2020-10-31 12:59:27 +0800

    Update wrapper.dart

--- lib/controller/wrapper.dart
+++ lib/controller/wrapper.dart
@@ -34,44 +34,49 @@ class _Wrapper extends State {
 
   Future<void> getRenderPage() async {
     bool loginState = await getLoginState();
-    var result;
-    try{
-      result = await APIService().getMethod('tokenValidation');
-    } finally{
-      //Token Validation Result
-    }
 
-    var message = json.decode(result.body);
     int role = await getRoleID();
     bool firstTimeState = await getFirstTimeState();
     if (firstTimeState != null) {
       if (loginState != null) {
-        if(message['error'] != null || message['success'] == true){
-          switch(role){
-            case 2:
-            case 3:
-            case 4:
-              Navigator.pushReplacementNamed(context, UserNavigationRoute);
-              break;
-            case 5:
-              Navigator.pushReplacementNamed(context, ManagementNavigationRoute);
-              break;
-            default:
+        APIService().getMethod('tokenValidation').then((response){
+          if(response.statusCode == 200){
+            if(response['error'] != null || response['success'] == true){
+              switch(role){
+                case 2:
+                case 3:
+                case 4:
+                  Navigator.pushReplacementNamed(context, UserNavigationRoute);
+                  break;
+                case 5:
+                  Navigator.pushReplacementNamed(context, ManagementNavigationRoute);
+                  break;
+                default:
+                  setLoginState(false).then((_) {
+                                  removePublicState();
+                                  removeUserState();
+                                  Navigator.of(context).pushReplacementNamed(AuthMainRoute);
+                                });
+                  break;
+              }
+            }
+            else{
+              //TODO: Show Loading API Call
               setLoginState(false).then((_) {
-                              removePublicState();
-                              removeUserState();
-                              Navigator.of(context).pushReplacementNamed(AuthMainRoute);
-                            });
-              break;
+                                  removePublicState();
+                                  removeUserState();
+                                  Navigator.of(context).pushReplacementNamed(AuthMainRoute);
+                                });
+            }
           }
-        }
-        else{
-          setLoginState(false).then((_) {
+          else{
+            setLoginState(false).then((_) {
                               removePublicState();
                               removeUserState();
                               Navigator.of(context).pushReplacementNamed(AuthMainRoute);
                             });
-        }
+          }
+        });
       } else {
         Navigator.pushReplacementNamed(context, AuthMainRoute);
       }
